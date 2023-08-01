8d42ae4baf5c16567c24767a38335f129f0344bb
alaverArt <alaverArt@yandex.ru>
2023-08-01 20:08:30 +0300

    Add all lending sections and connect backend

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,30 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(telegramConfig.botToken, {polling: true});
 
 let corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:8080'],
     //optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+function renderMessage(comfirmData) {
+    const dtFormatter = new Intl.DateTimeFormat('ru', { hour: '2-digit', minute: '2-digit' });
+    let result = '';
+    let userList = '';
+    let allUsersCount = 0;
+    let i = 1;
+
+    for(const [name, user] of Object.entries(comfirmData)) {
+        const countNum = Number(user.count);
+        const confirmDate = dtFormatter.format(new Date(user.confirmDate));
+
+        userList += `${i}. ${user.name} (${Math.abs(countNum)} чел., ${confirmDate ?? ''})\n\n`;
+        allUsersCount += Math.abs(countNum);
+
+        i++;
+    }
+
+    result += `<b>Количество гостей ${allUsersCount}</b>\n\n`;
+    result += userList;
+    return result;
 }
 
 class Application {
@@ -24,26 +46,61 @@ class Application {
 
     attachRoutes() {
         let app = this.expressApp;
-        app.post('/form', this.postInviteFormHandler.bind(this));
+        app.post('/confirm', this.postConfirmationHandler.bind(this));
     }
 
-    async postInviteFormHandler(req, res) {
+    async postConfirmationHandler(req, res) {
         const body = req.body;
         console.log(body);
-        let response = { status: 'success' };
+        let response = { status: 'success', err: null };
+        let data = null;
+
+        // read confirmation file
         try {
-            const data = await fs.promises.readFile('./formAnswers.json', { encoding: 'utf-8'});
-            const answers = JSON.parse(data);
-            answers[body.name.trim()] = { name: body.name, age: body.age };
-            await fs.promises.writeFile('./formAnswers.json', JSON.stringify(answers, null, 4), { encoding: 'utf-8' });
-            const msg = `
-                <b>Новый ответ</b>\n<b>Имя:</b> ${body.name.trim()}\n<b>Возраст:</b> ${body.age}\n
-            `;
-            bot.sendMessage(telegramConfig.chatId, msg, { parse_mode: 'HTML' });
+            data = await fs
+            .promises
+            .readFile('./confirm_data.json', { encoding: 'utf-8'});
         } catch (e) {
-            //console.error(e);
+            data = '{}';
+        }
+
+        try {
+            const answers = JSON.parse(data);
+            const name = body.name?.trim();
+            const count = Number(body.count ?? 0);
+            const confirmDate = new Date().getTime();
+            
+            if (!(name && count)) throw new Error('Не заполнено имя или количество человек');
+            
+            // write new confirmation
+            answers[name] = { name, count, confirmDate };
+            await fs
+                .promises
+                .writeFile(
+                    './confirm_data.json',
+                    JSON.stringify(answers, null, 4),
+                    { encoding: 'utf-8' }
+                );
+            
+            // delete last message if exists
+            if (telegramConfig.lastMessageId)
+                await bot.deleteMessage(telegramConfig.chatId, telegramConfig.lastMessageId);
+            
+            // send updated message
+            const msg = renderMessage(answers);
+            const newMsg = await bot.sendMessage(telegramConfig.chatId, msg, { parse_mode: 'HTML' });
+
+            // write new lastMessageId
+            telegramConfig.lastMessageId = newMsg.message_id;            
+            await fs.promises.writeFile(
+                './telegramConfig.json',
+                JSON.stringify(telegramConfig, null, 4),
+                { encoding: 'utf-8' }
+            );
+        } catch (e) {
             response.status = 'error';
             response.err = e.message;
+            res.json(response);
         }
         res.json(response);
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,34 @@ import '@/styles/main.scss';
 import '../index.html';
 import { createTimer } from './timer';
 
+const backendURL = 'http://localhost:3000';
+
+async function sendConfirm() {
+    const name = this.confirmInputName.value.trim();
+    const count = Number(this.confirmInputCount.value?.trim() ?? 1);
+    console.log(name, count);
+
+    if (!(name && count)) {
+        alert('Для подтверждения участия заполните имя и кол-во человек');
+        return;
+    }
+
+    try {
+        await fetch(backendURL + '/confirm', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify({
+                name,
+                count
+            }),
+        });
+    } catch(e) {
+        console.error(e);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const timerElem = document.querySelector('.timer');
     const timerMinValue = timerElem.querySelector('.m .value');
@@ -15,6 +43,12 @@ document.addEventListener('DOMContentLoaded', () => {
         timerDayValue.textContent = day;
         timerWeekValue.textContent = week;
     }
-    
+
     const tm = createTimer('2023-09-23 16:30:00 GMT+0300', updateTimerElem);
+
+    const confirmBtn = document.querySelector('.confirm__btn');
+    const confirmInputName = document.querySelector('.confirm__input.name');
+    const confirmInputCount = document.querySelector('.confirm__input.count');
+    
+    confirmBtn.addEventListener('click', sendConfirm.bidn({ confirmInputCount, confirmInputName }));
 })
