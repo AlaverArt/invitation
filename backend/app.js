bcf5b847d53fa8f05798272f8d57547eeabe425d
alaverArt <alaverArt@yandex.ru>
2023-08-03 19:20:39 +0300

    delete confirm, bugfixes

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,12 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(telegramConfig.botToken, {polling: true});
 
 let corsOptions = {
-    origin: ['http://95.163.234.38', 'https://95.163.234.38'],
+    origin: ['http://95.163.234.38', 'https://95.163.234.38', 'http://localhost:8080'],
     //optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 function renderMessage(comfirmData) {
-    const dtFormatter = new Intl.DateTimeFormat('ru', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' });
+    const dtFormatter = new Intl.DateTimeFormat('ru', { month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' });
     let result = '';
     let userList = '';
     let allUsersCount = 0;
@@ -25,7 +25,7 @@ function renderMessage(comfirmData) {
         const countNum = Number(user.count);
         const confirmDate = dtFormatter.format(new Date(user.confirmDate));
 
-        userList += `${i}. ${user.name} (${Math.abs(countNum)} чел., ${confirmDate ?? ''})\n\n`;
+        userList += `${i}. ${user.name}\n[ ${Math.abs(countNum)} чел. ]    ${confirmDate ?? ''}\n\n`;
         allUsersCount += Math.abs(countNum);
 
         i++;
@@ -47,12 +47,79 @@ class Application {
     attachRoutes() {
         let app = this.expressApp;
         app.post('/confirm', this.postConfirmationHandler.bind(this));
+        app.post('/delete-confirm', this.postDeleteConfrimationHandler.bind(this));
+    }
+
+    async postDeleteConfrimationHandler(req, res) {
+        const body = req.body;
+        console.log(body);
+        const name = body.name?.trim();
+        let response = { errCode: null, errMsg: null };
+        let data = null;
+        let newMsg = null;
+        // read confirmation file
+        try {
+            data = await fs
+            .promises
+            .readFile('./confirm_data.json', { encoding: 'utf-8'});
+        } catch (e) {
+            data = '{}';
+        }
+        let answers = JSON.parse(data);
+        if (answers[name]) {
+            delete answers[name];
+            await fs
+                .promises
+                .writeFile(
+                    './confirm_data.json',
+                    JSON.stringify(answers, null, 4),
+                    { encoding: 'utf-8' }
+                );
+
+            // delete last message if exists
+            if (telegramConfig.lastMessageId)
+            try {
+                await bot.deleteMessage(telegramConfig.chatId, telegramConfig.lastMessageId);
+            } catch (e) {
+                console.log(e);
+            }
+            // send updated message
+            try {
+                const msg = renderMessage(answers);
+                newMsg = await bot.sendMessage(telegramConfig.chatId, msg, { parse_mode: 'HTML' });
+
+                // send file for safety
+                await bot.sendDocument(telegramConfig.backupChatId, 'confirm_data.json', {}, { contentType: 'application/json' });
+
+                // write new lastMessageId
+                telegramConfig.lastMessageId = newMsg?.message_id ?? null;            
+                await fs.promises.writeFile(
+                    './telegramConfig.json',
+                    JSON.stringify(telegramConfig, null, 4),
+                    { encoding: 'utf-8' }
+                );
+
+            } catch (e) {
+                try {
+                    await bot.sendMessage(telegramConfig.backupChatId, `УДАЛЕНИЕ \n\n${e.message}\n\n${e.sourceErrMsg}`, { parse_mode: 'HTML' });
+                } catch (err) {
+                    console.log(err);
+                }
+
+                response.errCode = e.code;
+                response.errMsg = e.message;
+                res.status(200).json(response);
+                return;
+            }
+        }
+
+        res.status(200).json(response);
     }
 
     async postConfirmationHandler(req, res) {
         const body = req.body;
         console.log(body);
-        let response = { status: 'success', err: null };
+        let response = { errCode: null, errMsg: null };
         let data = null;
 
         // read confirmation file
@@ -69,38 +136,86 @@ class Application {
             const name = body.name?.trim();
             const count = Number(body.count ?? 0);
             const confirmDate = new Date().getTime();
+            let newMsg = null;
             
-            if (!(name && count)) throw new Error('Не заполнено имя или количество человек');
-            
-            // write new confirmation
-            answers[name] = { name, count, confirmDate };
-            await fs
-                .promises
-                .writeFile(
-                    './confirm_data.json',
-                    JSON.stringify(answers, null, 4),
-                    { encoding: 'utf-8' }
-                );
+            if (!(name && count)) {
+                const newErr = new Error('Не заполнено имя или количество человек');
+                newErr.code = 'empty_name';
+                newErr.sourceErrMsg = null;
+                throw newErr;
+            }
+            if (answers[name]) {
+                const newErr = new Error('Человек с таким ФИО уже зарегистрирован. Если это вы, то вы можете отменить регистрацию с того же устройства, на котором она производилась, и зарегистрироваться заново');
+                newErr.code = 'exist_name';
+                newErr.sourceErrMsg = null;
+                throw newErr;
+            }
+
+            try {
+                // write new confirmation
+                answers[name] = { name, count, confirmDate };
+                await fs
+                    .promises
+                    .writeFile(
+                        './confirm_data.json',
+                        JSON.stringify(answers, null, 4),
+                        { encoding: 'utf-8' }
+                    );
+            } catch (err) {
+                const newErr = new Error('Не удалось подтвердить участие');
+                newErr.code = 'unknown';
+                newErr.sourceErrMsg = err.message;
+                throw newErr;
+            }
             
             // delete last message if exists
             if (telegramConfig.lastMessageId)
-                await bot.deleteMessage(telegramConfig.chatId, telegramConfig.lastMessageId);
+                try {
+                    await bot.deleteMessage(telegramConfig.chatId, telegramConfig.lastMessageId);
+                } catch (e) {
+                    console.log(e);
+                }
             
             // send updated message
-            const msg = renderMessage(answers);
-            const newMsg = await bot.sendMessage(telegramConfig.chatId, msg, { parse_mode: 'HTML' });
+            try {
+                const msg = renderMessage(answers);
+                newMsg = await bot.sendMessage(telegramConfig.chatId, msg, { parse_mode: 'HTML' });
+
+                // send file for safety
+                await bot.sendDocument(telegramConfig.backupChatId, 'confirm_data.json', {}, { contentType: 'application/json' });
+            } catch (e) {
+                // undo file changes
+                delete answers[name];
+                await fs
+                    .promises
+                    .writeFile(
+                        './confirm_data.json',
+                        JSON.stringify(answers, null, 4),
+                        { encoding: 'utf-8' }
+                    );
+                const newErr = new Error('Не удалось подтвердить участие');
+                newErr.code = 'unknown';
+                newErr.sourceErrMsg = e.message;
+                throw newErr;
+            }
 
             // write new lastMessageId
-            telegramConfig.lastMessageId = newMsg.message_id;            
+            telegramConfig.lastMessageId = newMsg?.message_id ?? null;            
             await fs.promises.writeFile(
                 './telegramConfig.json',
                 JSON.stringify(telegramConfig, null, 4),
                 { encoding: 'utf-8' }
             );
         } catch (e) {
-            response.status = 'error';
-            response.err = e.message;
-            res.status(400).json(response);
+            try {
+                await bot.sendMessage(telegramConfig.backupChatId, `${e.message}\n\n${e.sourceErrMsg}`, { parse_mode: 'HTML' });
+            } catch (err) {
+                console.log(err);
+            }
+
+            response.errCode = e.code;
+            response.errMsg = e.message;
+            res.status(404).json(response);
             return;
         }
         res.status(200).json(response);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,11 @@ import '@/styles/main.scss';
 import '../index.html';
 import { createTimer } from './timer';
 
-const backendURL = 'http://95.163.234.38:3000';
+const backendURL = 'http://localhost:3000';
 
 async function sendConfirm() {
     const name = this.confirmInputName.value.trim();
     const count = Number(this.confirmInputCount.value?.trim() ?? 1);
-    console.log(name, count);
 
     if (!(name && count)) {
         alert('Для подтверждения участия заполните имя и кол-во человек');
@@ -15,7 +14,9 @@ async function sendConfirm() {
     }
 
     try {
-        await fetch(backendURL + '/confirm', {
+        this.msgContainer.classList.add('hidden');
+        this.confirmBtn.classList.add('loading');
+        let resp = await fetch(backendURL + '/confirm', {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json;charset=utf-8',
@@ -25,8 +26,57 @@ async function sendConfirm() {
                 count
             }),
         });
+
+        if (resp.status !== 200) {
+            throw await resp.json();
+        }
+
+        localStorage.setItem('confirm', JSON.stringify({ name, count }));
+        checkUserConfirm.bind(this)();
     } catch(e) {
-        console.error(e);
+        console.log(e);
+        handleConfirmError.bind(this)(e.errCode, e.errMsg);
+    } finally {
+        this.confirmBtn.classList.remove('loading');
+    }
+}
+
+function handleConfirmError(code, msg) {
+    this.msgTextContainer.textContent = msg || 'Не удалось подтвердить. Обновите страницу и попробуйте снова';
+    this.msgContainer.classList.remove('hidden');
+    this.deleteConfirmBtn.classList.add('hidden');
+}
+
+function checkUserConfirm() {
+    let data = localStorage.getItem('confirm');
+    if (!data) return;
+    data = JSON.parse(data);
+
+    this.msgTextContainer.textContent = `Вы подтвердили своё участие: ${data.name} ${data.count} чел.`;
+    this.msgItemConfirm.classList.add('hidden');
+    this.deleteConfirmBtn.classList.remove('hidden');
+
+    this.msgContainer.classList.remove('hidden');
+}
+
+async function sendDeleteConfrim() {
+    const data = localStorage.getItem('confirm');
+    if (!data) return;
+    try {
+        this.deleteConfirmBtn.classList.add('loading');
+        await fetch(backendURL + '/delete-confirm', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json;charset=utf-8',
+            },
+            body: data,
+        });
+        localStorage.removeItem('confirm');
+        window.location.reload();
+    } catch(e) {
+        console.log(e);
+    } finally {
+        this.deleteConfirmBtn.classList.remove('loading');
     }
 }
 
@@ -46,9 +96,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const tm = createTimer('2023-09-23 16:30:00 GMT+0300', updateTimerElem);
 
-    const confirmBtn = document.querySelector('.confirm__btn');
-    const confirmInputName = document.querySelector('.confirm__input.name');
-    const confirmInputCount = document.querySelector('.confirm__input.count');
-    
-    confirmBtn.addEventListener('click', sendConfirm.bind({ confirmInputCount, confirmInputName }));
-})
+    const elems = {};
+    elems.confirmBtn = document.querySelector('.confirm__btn');
+    elems.confirmInputName = document.querySelector('.confirm__input.name');
+    elems.confirmInputCount = document.querySelector('.confirm__input.count');
+
+    elems.msgContainer = document.querySelector('.confirm-message-container');
+    elems.msgTextContainer = elems.msgContainer.querySelector('.msg-text');
+    elems.deleteConfirmBtn = elems.msgContainer.querySelector('.delete-confirm-button');
+    elems.msgItemConfirm = document.querySelector('.message__item.confirm');
+
+    checkUserConfirm.bind(elems)();
+
+    elems.confirmBtn.addEventListener('click', sendConfirm.bind(elems));
+    elems.deleteConfirmBtn.addEventListener('click', sendDeleteConfrim.bind(elems));
+});
